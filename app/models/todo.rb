010ef71a3437ea87935c6c99b8b0ea2a39ea5c80
Boakye2-Yiadom-Nana <boakyeyiadomnana99@gmail.com>
2025-11-17 21:36:40 +0000

    api commit

---
+++ app/models/todo.rb
@@ -0,0 +1,21 @@
+class Todo < ApplicationRecord
+    validates :title, presence: true
+    validates :completed, inclusion: { in: [true, false] }
+    validates :description, presence: true
+    validates :description, length: { minimum: 10 }
+    validates :title, length: { maximum: 100 }
+
+    validate :title_cannot_be_completed_if_empty
+
+    
+end
+
+def completed?
+    completed
+end
+
+def title_cannot_be_completed_if_empty
+    if title.blank? && completed?
+        errors.add(:title, "cannot be empty if the todo is marked as completed")
+    end
+end
